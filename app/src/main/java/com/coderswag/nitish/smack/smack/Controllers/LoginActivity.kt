b05622aef094c7c06d2c1c965b0cc1922d01cd04
Nitish Rana <nitish@maropost.com>
2018-03-21 14:59:26 +0530

    Created login screen

---
+++ app/src/main/java/com/coderswag/nitish/smack/smack/Controllers/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.coderswag.nitish.smack.smack.Controllers
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.coderswag.nitish.smack.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginButtonClicked(view: View) {
+
+    }
+
+    fun loginCreateButtonClicked(view: View) {
+
+    }
+}
