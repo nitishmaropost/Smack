8aa4139f57bc76b87d834d16d950ecd71579700e
Nitish Rana <nitish@maropost.com>
2018-03-28 14:18:51 +0530

    Added Volley and constants

---
+++ app/src/main/java/com/coderswag/nitish/smack/smack/Services/AuthService.kt
@@ -0,0 +1,4 @@
+package com.coderswag.nitish.smack.smack.Services
+
+object AuthService {
+}
---
+++ app/src/main/java/com/coderswag/nitish/smack/smack/Utilities/Constants.kt
@@ -0,0 +1,4 @@
+package com.coderswag.nitish.smack.smack.Utilities
+
+const val BASE_URL = "https://testchattychat.herokuapp.com/v1/"
+const val REGISTER_URL = "${BASE_URL}account/register"
